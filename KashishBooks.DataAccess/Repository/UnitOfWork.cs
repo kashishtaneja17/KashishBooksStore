5d1d2f4fa3fcfba4fc2b8aa122fb6d486ae51d85
W0834114 <W0834114@myscc.ca>
2023-12-05 12:15:09 -0500

    modifytheClassUnitOfWork.cs

---
+++ KashishBooks.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using KashishBooks.DataAccess.Repository.IRepository;
+using KashishBooksStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashishBooks.DataAccess.Repository
+{
+    public class UnitOfWork 
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+       
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
