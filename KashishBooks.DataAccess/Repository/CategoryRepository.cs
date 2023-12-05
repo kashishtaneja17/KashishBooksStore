01563b93c90e71b6df3a9a280fe918c82fd99d9c
W0834114 <W0834114@myscc.ca>
2023-12-04 21:39:27 -0500

    CompleteTheRemainingModificationofCategory.cs

--- KashishBooks.DataAccess/Repository/CategoryRepository.cs
+++ KashishBooks.DataAccess/Repository/CategoryRepository.cs
@@ -17,5 +17,15 @@ namespace KashishBooks.DataAccess.Repository
         {
             _db = db;
         }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
     }
 }
