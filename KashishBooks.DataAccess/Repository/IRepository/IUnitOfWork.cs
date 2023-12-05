bd4d5d684567121a31d117fa00e8a1a8fd9b2e34
W0834114 <W0834114@myscc.ca>
2023-12-05 11:44:54 -0500

    CreateanInterfaceNamedasIUnitOfWork.cs

---
+++ KashishBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashishBooks.DataAccess.Repository.IRepository
+{
+    public interface UnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        ISP_Call SP_Call { get; }
+    }
+}
