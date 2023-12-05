98b93fdafa1fc76d3cf665d21c4593f8ce163be6
W0834114 <W0834114@myscc.ca>
2023-12-05 15:16:56 -0500

    addingInterfaceandClassIntoRepository.cs

---
+++ KashishBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using KashishBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashishBooks.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
