0dac6783757b85f29f570be031f60b0b85eeb5eb
W0834114 <W0834114@myscc.ca>
2023-12-04 21:36:30 -0500

    modifythenewinterfaceIcategoryRepository

---
+++ KashishBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using KashishBooks.DataAccess.Repository.IRepository;
+using KashishBooks.Models;
+using KashishBooksStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashishBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
