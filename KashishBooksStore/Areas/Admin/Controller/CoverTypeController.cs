e7bbc121961e84eeae812674d70c4f2c804bfa46
W0834114 <W0834114@myscc.ca>
2023-12-05 14:32:29 -0500

    addinGcOVERtYPEcONTROLLER.CS

---
+++ KashishBooksStore/Areas/Admin/Controller/CoverTypeController.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using KashishBooks.DataAccess.Repository.IRepository;
+using KashishBooks.Models;
+using KashishBooks.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KashishBooksStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
