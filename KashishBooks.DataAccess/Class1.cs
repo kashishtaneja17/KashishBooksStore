37815e0b6ee5c5bcf57575a0d1eb20072d703fe3
W0834114 <W0834114@myscc.ca>
2023-12-04 19:37:50 -0500

    addingNewProjectsAndNamedAsDataAccess,ModelsAndUtility

---
+++ KashishBooks.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KashishBooks.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ KashishBooks.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KashishBooks.Models
+{
+    public class Class1
+    {
+    }
+}
